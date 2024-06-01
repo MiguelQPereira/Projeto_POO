f01824f4c02b3f1d151aecc2ccc0d4f4c7148eea
MiguelQPereira <miguelquinapereira@tecnico.ulisboa.pt>
2024-06-01 13:48:42 +0100

    java doc do Simulation

--- projeto/dss/Simulation.java
+++ projeto/dss/Simulation.java
@@ -4,14 +4,23 @@ package dss;
  * It scheduals and executes the events.
  */
 public class Simulation implements Sim{
-    
+    /** Maximum simulation time. */
     private final int t;
+
+    /** Number of events processed. */
     private int numEvs;
 
+    /** Current simulation time. */
     private double instante;
 
+    /** Event queue. */
     private PecInt Evs;
 
+    /**
+     * Constructs a Simulation object with a specified maximum time
+     * 
+     * @param t Maximum simulation time.
+     */
     public Simulation(int t) {
 
         this.t = t;
@@ -19,6 +28,9 @@ public class Simulation implements Sim{
         this.Evs = Pec.getPec();
     }
 
+    /**
+     * Runs the simulation by processing events until the event queue is empty.
+     */
     public void runSim() {
 
         while (!Evs.isEmpty()) {
@@ -35,6 +47,11 @@ public class Simulation implements Sim{
         return ;
     }
 
+    /**
+     * Adds an event to the simulation if there is time to be processed.
+     * 
+     * @param ev Event to add.
+     */
     public void addEv(Event ev) {
 
         if (ev.getTime() > this.t || ev.getTime() < 0) return;
@@ -44,18 +61,36 @@ public class Simulation implements Sim{
         return;
     }
 
+    /**
+     * Gets the current simulation time.
+     * 
+     * @return Current simulation time.
+     */
     public double getTime() {
         return this.instante;
     }
 
+    /**
+     * Gets the maximum simulation time.
+     * 
+     * @return Maximum simulation time.
+     */
     public int getMaxTime() {
         return this.t;
     }
 
+    /**
+     * Gets the number of events processed.
+     * 
+     * @return Number of events processed.
+     */
     public int getEvsNum() {
         return this.numEvs;
     }
 
+    /**
+     * Empties the event queue, removing all scheduled events.
+     */
     public void emptiesPec() {
 
         this.Evs.empties();
@@ -63,6 +98,9 @@ public class Simulation implements Sim{
         return;
     }
 
+    /**
+     * Cleans the event queue, removing processed events.
+     */
     public void cleanPec() {
 
         this.Evs.clean();
